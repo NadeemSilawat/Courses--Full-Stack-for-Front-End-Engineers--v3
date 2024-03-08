268f50d2b1ce60136532424f3cc365050828cae0
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-08 12:44:28 +0530

    Server

---
+++ 02-server.js
@@ -0,0 +1,39 @@
+
+            /********* Server ************/
+
+/*
+    * What is a server
+    * Cloud Computing
+    * VPS
+    * Operating System
+    * SSH
+*/        
+
+
+//Basic node server 
+        // Make a file called simpleServer.js
+        // Creating a simple HTTP server using Node.js running on port 3000
+
+
+    /*
+        const http = require("http");
+        const fs = require('fs');
+        consr PORT = 3000;
+
+        const server = http.createServer(function (req, res) {
+            res.writeHead(200, {'content-type': 'text/html'});
+            fs.createReadStream('index.html').pipe(res);
+        });
+        
+        server.listen(PORT);
+        console.log(`Server started on port ${PORT}`);
+
+            
+
+        //Create a HTML file in vim
+
+        // Save it as index.html
+        
+    */ 
+   
+        
