2dc6334466c55df5ea5cf2a9e8caf9c8dc6bd131
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-09 11:19:04 +0530

    Operating System

---
+++ 03-operating-system.js
@@ -0,0 +1,25 @@
+
+                /******* Operating System *********/
+
+// Operating System 
+        /*
+            1. User
+            2. Shell 
+       ____ 3. Kernal_______
+      |____ 4. Hardware_____|
+
+            */            
+
+
+                /********* Security & Hashing *********/
+
+// Security 
+        
+        /*
+            * Username / Password
+            * Biomatric
+            * SSH Key
+        */
+
+
+        
