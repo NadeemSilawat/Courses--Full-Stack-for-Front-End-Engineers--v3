9a60f0eef28f51ae7ac2f064b2d8f2fa8a6ee6ec
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-10 15:18:01 +0530

    Operating System

--- 03-operating-system.js
+++ 03-operating-system.js
@@ -37,5 +37,18 @@
             // MD5(foo)= 286755fad04869ca523320acce0dc6a4
     */
 
+                // Hasing With Salt
 
+// command
+    /*
+        ~ temp openssl md5 foo | awk 
+        ~ temp openssl md5 foo | awk -F'{print $2}' 
+        ~ temp openssl sha1 foo 
+        SHA1(foo)= c8fed00eb2e87f1cee8e90ebbe870c190ac3848c
+        ~ temp openssl sha256 foo                           ----> 256 - 65 Character long
+        SHA2-256(foo)= 6b3a55e0261b0304143f805a24924d0c1c44524821305f31d9277843b8a10f4e                 
+
+    */        
             
+
+        
