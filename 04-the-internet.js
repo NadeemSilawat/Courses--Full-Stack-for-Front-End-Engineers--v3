4bcb06ce673a7e2739f5bf4c086acf9cd2d4ca69
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-11 22:19:31 +0530

    The Internet

--- 04-the-internet.js
+++ 04-the-internet.js
@@ -12,3 +12,16 @@
         /********* How to Internet Works ************/
 
         
+//The internet is built on cooperation and rules
+
+    /*
+        computer
+            |
+        network card                        Server
+            |                                 /|\          
+          router                          Load balancer
+            |                                 /|\ 
+           ISP                             Server cluster
+            |                                 /|\  
+        Tier 1 ISP ---> Tier 1 ISP -----> Datacenter
+    */
