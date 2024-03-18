51c687da2f8d0c2efe5895de69fb3cec54698474
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-18 23:21:36 +0530

    Server

--- 02-server.js
+++ 02-server.js
@@ -9,6 +9,9 @@
     * SSH
 */        
 
+//What is Server ?
+    // A server is a computer or system that provides resources, data, services, or programs to other computers, known as clients, over a network. In theory, whenever computers share resources with client machines they are considered servers.   
+
 
 //Basic node server 
         // Make a file called simpleServer.js
