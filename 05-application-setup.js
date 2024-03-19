b0e097558038a618b8cf4cba29612e055eda5064
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-19 15:27:58 +0530

    Application Setup

--- 05-application-setup.js
+++ 05-application-setup.js
@@ -1,2 +1,14 @@
 
         /******* Setup Nginx Web Server ***********/    
+
+
+// What is Web Server ?
+
+        //A computer program that stores, porcesses , and delivers web pages to users.
+
+
+// Web Server 
+
+        // * Apache  * Nginx
+
+        
