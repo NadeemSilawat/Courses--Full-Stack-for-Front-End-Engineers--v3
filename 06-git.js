613e09cbe45574898fd73d03e08b720bb58bf1f0
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-20 16:31:28 +0530

    GIT

--- 06-git.js
+++ 06-git.js
@@ -1,1 +1,48 @@
         /******** Git Exercise **********/
+
+//Version Control 
+
+        /**
+                * version control -> record changes to a file system to preserve history
+                
+                * git subversion mercurial 
+         */
+
+
+
+        /****** Git ***********/
+
+//Exercise Git
+
+
+/*
+        1. create a git repository
+        2. On your server, create an SSH key called gh_key
+        3. Add the SSH key to Github
+        4. Add remote repo 
+        5. Ensure new SSH key is used
+        6. Push local repository to Github
+
+
+*/
+
+/*
+        Git 
+     ----------
+
+                1. Ensure git uses your new ssh key
+                        vi ~/.ssh/config
+                2. Changes Permission of config to 600
+                        chmod 600 ~/.ssh/config
+                3. Changes Permission of gh_key to 600
+                        chmod 600 ~/.ssh/gh_key
+
+                
+                HOST -> github.com
+                HOSTNAME -> github.com
+                IDENTITYFILE -> ~/.ssh/gh_key
+        
+*/
+
+
+
