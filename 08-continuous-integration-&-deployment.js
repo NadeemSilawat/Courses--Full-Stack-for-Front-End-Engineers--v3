5549bcb75af9f4f3443f8ba1e32e7376dae056b2
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-22 16:25:31 +0530

    Continuous Integration & deployment

---
+++ 08-continuous-integration-&-deployment.js
@@ -0,0 +1,25 @@
+
+/*
+    1. CL/CD
+    2.Shell scripting
+    3. Cron
+
+*/
+
+
+
+                        /******** CL/CD **********/
+
+    /*
+        CL/CD
+       --------
+       
+       Continuous integration -> Code changes are validated and merged back into
+                                main branch as often as possible.
+
+       Continuous delivery -> Code changes are automatically built and 
+                                ready for production.
+
+       Continuous deployment -> Builds are automatically deployed 
+                                to production enviroments.                                
+    */                        
