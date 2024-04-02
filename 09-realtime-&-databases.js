2efdb77c860b9b3b7e28c9781bf77a2e8cfe4ae0
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-04-02 16:10:35 +0530

    HTTP & Status Code

--- 09-realtime-&-databases.js
+++ 09-realtime-&-databases.js
@@ -64,3 +64,47 @@ Exercise : websocket
                 * Data agnostic
                 * Loose structure
         */
+
+/*
+        HTTP (Hypertext Transfer Protocol)
+    ------------
+
+                -------------------->
+        request                         response           
+                <--------------------
+
+
+                
+  Headers
+    
+                    Common headers
+                 --------------------
+    * User-agent     ->     The requesting device type
+    * Accept         ->     What the device will handle
+    * Accept-language->     Browser language
+    * Content-type   ->     The type of media
+    * Set-cookie     ->     Sets stateful information
+    * X-             ->     Typically used for custom headers
+                     
+
+ Status Code 
+        
+        * 200       Ok
+        * 301       Moved permanently
+        * 302       Found (temporary redirct)
+        * 401       Not authorized
+        * 500       Internal server error 
+        * 404       Page Not Found
+
+    Status Code -> indicates status of an http resuest 
+
+
+        * 1xx       Information
+        * 2xx       Success
+        * 3xx       Redirect 
+        * 4xx       Client error
+        * 5xx       Server error
+
+    Note :- What is the proper status code for a successful POST request?
+        
+ */          
