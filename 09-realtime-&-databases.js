5efd64bd28d66bd2ada60d255773a306e6d1559f
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-04-02 16:31:05 +0530

    Realtime & Database

--- 09-realtime-&-databases.js
+++ 09-realtime-&-databases.js
@@ -106,5 +106,94 @@ Exercise : websocket
         * 5xx       Server error
 
     Note :- What is the proper status code for a successful POST request?
+        ans -> 201 (create successfull)
         
+ HTTPS :-
+
+        1.) yourwebsite.com -------> visa 4235529596901600 -------> server
+                                password 123455
+
+
+                                    EVIL server
+        2.) yourwebsite.com -------> visa 4235529596901600 -------> server                                
+
+
+
+        2.) yourwebsite.com -------> *Ifkfi24fh243&&o9#$kD#!8b -------> server
+
+
  */          
+        /******** (Implementing HTTPS with Certbot) ************/
+
+
+/*
+    HTTPS
+
+        Certbot is an easy-to-use client that fetches a certificate from Let's Encrypt—an open certificate
+         authority launched by the EFF, Mozilla, and others—and deploys it to a web server.
+
+            1.) INstall and use certbot
+            2.) Open firewall
+
+            // https://certbot.eff.org/instructions
+
+            // terminal
+
+            sudo snap install core; sudo snap refresh core
+            sudo apt-get remove certbot
+            sudo snap install --classic certbot     // error - work painding
+            sudo ln -s 
+            man ln
+            sudo certbot --nginx    // enter email  Y, N
+            sudo cat /etc/nginx/sites-enabled/fsfe
+            sudo certbot renew --dry-run
+            clear
+
+            sudo ufw allow https
+            sudo ufw status
+            
+ */        
+
+/*
+
+         (Supporting HTTP/2)
+
+         HTTP/2
+        
+                            http/1.1
+        
+        main.js  ----TCP----->
+                 <------------
+                 
+        index.html  ----TCP--->         SERVER
+                <--------------
+                
+        dog.png  ----TCP------>
+                 <-------------               
+                 
+
+                            http/2
+
+        main.js
+        
+        index.html  -----TCP----->      SERVER
+                    <-------------
+        dog.png                   
+        
+        
+        method      status      protocol        scheme
+        GET         200           h2             https
+        GET         200           h2             https
+
+
+        1.) Update nginx server
+        // sudo vi /etc/nginx/sites-enabled/fsfe
+                                |
+                               \|/
+                  liisten 443 http2 ssl;
+
+
+    sudo vi /etc/nginx/sites-enabled/fsfe   // some changes in file
+    sudo nginx -t
+    sudo service nginx restart
+*/ 
