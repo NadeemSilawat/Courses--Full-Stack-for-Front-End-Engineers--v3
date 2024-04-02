33d506072bd74b73c8f25f889c392bfe3e985564
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-04-02 16:20:54 +0530

    Container & Docker

---
+++ 10-container.js
@@ -0,0 +1,73 @@
+/********* (Containers) ************/ 
+
+// Containers
+
+/*
+    => Microservices
+    => Containers
+    => Orchestration
+    => Load balancers
+    => Deployments
+
+    Microservices :- Software architecture of loosely connected services.
+
+    Monolith :- Software architecture of tightly coupled services.
+    // easy and only one language
+
+    // VPS :- virtual private server.
+
+
+    // containers
+    |----------|                        ubuntu
+    |----------|                        NodeJS
+    |----------|                        MySQL
+    |          |                        Nginx
+    |     |    |                        Python
+    |     .    |
+    |          |
+    ------------
+
+
+
+    // containers
+    |----------|                            ubuntu
+    |----------|                        NodeJS  MySQL
+    |----------|                        
+    |          |                        Nginx   Python
+    |     |    |   ------------->          
+    |     .    |
+    |          |                        // in one container
+    ------------
+
+
+
+        OSX                                 Fedora
+
+    NodeJS  MySQL                       NodeJS  MySQL
+    
+    Nginx   Python                      Nginx   Python
+
+    // multiple container
+
+
+   Defination 
+ ----------------   
+    Dockers :- Docker is a software platform that allows you to build, test, and deploy applications quickly.
+               Docker packages software into standardized units called containers that have everything the 
+               software needs to run including libraries, system tools, code, and runtime.
+
+    1.) Amazon ECS
+    2.) Apache
+    3.) CoreOS rkt
+
+
+    // containers :-
+
+    => Lightweight
+    => portable
+    => easier for development
+    => easier to manage
+    => faster startup
+    => Decouple application from infrastructure
+
+*/ 
