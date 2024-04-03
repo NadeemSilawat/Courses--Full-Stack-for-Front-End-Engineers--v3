d777ae27d34587bebb92da15dbec9a15d8163f98
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-04-03 15:46:12 +0530

    Docker Container

--- 10-container.js
+++ 10-container.js
@@ -56,7 +56,7 @@
                Docker packages software into standardized units called containers that have everything the 
                software needs to run including libraries, system tools, code, and runtime.
 
-    1.) Amazon ECS
+    1.) Amazon ECS (Elastic Container Service)
     2.) Apache
     3.) CoreOS rkt
 
@@ -71,3 +71,40 @@
     => Decouple application from infrastructure
 
 */ 
+
+
+    // Creating a Docker Container
+
+/*
+ 1.) create a docker contaimer for our app
+      https://github.com/young/fullstack-for-frontend3/blob/main/apps/simpleServer/Dockerfile
+
+         terminal
+
+    pwd
+    cd /var/www/app/
+    clear
+
+    vi Dockerfile
+    docker
+    sudo apt install docker.io      // y
+    docker
+    clear
+
+    docker build -t node-fsfe
+    docker build -t node-fsfe .
+    sudo !!
+
+    search in browser node docker (19 alpine, 3.6)
+
+    docker image
+    docker image ls
+    sudo !!
+    sudo docker run -d -p 3000:3000 node-fsfe
+    pm2 stop index_ws.js    // error
+    sudo docker run -d -p 8080:8080 node-fsfe
+    docker ps
+    sudo !!
+
+*/    
+
